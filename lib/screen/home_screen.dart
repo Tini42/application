ca02b91798d96d11e1ba1f48063a9e0ff644b5a3
Tini42 <diepnguyenminhtuyen2002@gmail.com>
2024-10-29 22:55:23 +0700

    giao dien

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -21,7 +21,7 @@ class _HomeScreenState extends State<HomeScreen> {
             child: Text(
               "VĂN HÓA VIỆT NAM",
               style: TextStyle(
-                fontSize: 25,
+                fontSize: 30,
                 fontWeight: FontWeight.bold,
                 color: Colors.black,
               ),
